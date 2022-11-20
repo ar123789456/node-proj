d77177d86899bc82054dc812ec2cb00dd59a2bf4
ar123789456 <ar123789456@mail.ru>
2022-11-20 16:27:56 +0600

    todos repository

--- repository/mongo.ts
+++ repository/mongo.ts
@@ -1,5 +1,5 @@
 import {User} from "../models/models";
-import {RepositoryAuth} from "../models/interface";
+import {RepositoryAuth, RepositoryTodo} from "../models/interface";
 import mongoose, {Promise} from "mongoose";
 
 //Auth repository
@@ -41,6 +41,55 @@ export class AuthRepository implements RepositoryAuth {
             this.model.findOneAndUpdate({
                     name: user.name,
                 }, user, (err, result) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(result);
+                }
+            )
+        });
+    }
+}
+
+//Todos repository
+export class TodosRepository implements RepositoryTodo {
+    model: mongoose.Model<mongoose.Document>;
+
+    constructor(model: mongoose.Model<mongoose.Document>) {
+        this.model = model;
+    }
+
+    get(filter: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.model.find({
+                    status: filter.status,
+                    title: new RegExp(filter.search, "i")
+                }, (err, result) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(result);
+                }
+            ).skip(filter.startIndex).limit(filter.endIndex - filter.startIndex);
+        });
+    }
+
+    set(todo: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.model.create(todo, (err, result) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(result);
+            });
+        });
+    }
+
+    update(todo: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.model.findOneAndUpdate({
+                    _id: todo._id,
+                }, todo, (err, result) => {
                     if (err) {
                         reject(err);
                     }
